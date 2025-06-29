d57f7073090d859fa5a62545c9d7977ddb383ea7
Sergio Andres Picon Sanchez <126985792+sergio-010@users.noreply.github.com>
2025-06-29 09:35:42 -0500

    Improve button visibility

--- src/app/admin/page.tsx
+++ src/app/admin/page.tsx
@@ -10,7 +10,7 @@ export default async function AdminPage() {
             <h1 className="text-2xl font-bold mb-6">Panel de Administración</h1>
             <Link
                 href="/admin/new"
-                className="inline-block mb-6 bg-gold text-black px-4 py-2 rounded-md font-medium shadow hover:bg-yellow-400 transition"
+                className="inline-block mb-6 bg-gold text-black text-lg px-4 py-2 rounded-md font-medium border border-black/20 shadow-lg hover:bg-yellow-400 transition"
             >
                 + Agregar producto
             </Link>
